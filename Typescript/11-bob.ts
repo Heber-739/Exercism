e610852cbc84873d47f14a5beab8e6547a42ec72
Heber-739 <Heberduarteryr@gmail.com>
2023-02-13 15:19:02 -0300

    12th excercism typescript

--- Typescript/11-bob.ts
+++ Typescript/11-bob.ts
@@ -7,29 +7,22 @@ El compañero de conversación de Bob es un purista en lo que respecta a la comu
 sigue las reglas normales con respecto a la puntuación de las oraciones en inglés.
  */
 
-export function hey(message: string): string {
-  if (message.length < 1) {
-    return "Fine. Be that way!";
-  }
-  let question: boolean = false;
-  let wordsCapitals: number = 0;
-  let len = message.length;
-  if (message[len] === "?") {
-    question = true;
-    console.log(question);
-  }
-  for (let i = 0; i < message.length; i++) {
-    if (message[i].match(/[A-Z]/)?.length == 1) {
-      wordsCapitals++;
+export function hey(mess: string): string {
+  let message: string = mess.trim();
+  if (message.match(/\S/)) {
+    if (message.match(/[a-z0-9]+[?]$/) || message.match(/[OK][?]$/)) {
+      return "Sure.";
     }
+    if (message.match(/[^a-z]+[a-zA-Z]+[?]$/g)) {
+      return "Calm down, I know what I'm doing!";
+    }
+    if (message.match(/[a-zA-Z]/g)) {
+      if (message.match(/[A-Z]+[!]/g) || message == message.toUpperCase()) {
+        return "Whoa, chill out!";
+      }
+    }
+    return "Whatever.";
   }
-  if (question && wordsCapitals < 2) {
-    return "Sure.";
-  } else if (!question && wordsCapitals >= 2) {
-    return "Whoa, chill out!";
-  } else if (question && wordsCapitals >= 2) {
-    return "Calm down, I know what I'm doing!";
-  }
-  return "Whatever.";
+  return "Fine. Be that way!";
 }
 hey("ajshdkhlsks?");
