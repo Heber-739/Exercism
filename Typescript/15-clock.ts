e5cd8d6cfd340003f648bdbf490d552516ba90d2
Heber-739 <Heberduarteryr@gmail.com>
2023-02-17 12:35:04 -0300

    16th excercism typescript

--- Typescript/15-clock.ts
+++ Typescript/15-clock.ts
@@ -1,10 +1,12 @@
 export class Clock {
-  private minuts: number = 0;
-  private hours: number = 0;
+  private minuts: number;
+  private hours: number;
 
   constructor(hour: number, minute?: number) {
-    this.minuts = minute || 0;
-    this.hours = hour;
+    this.minuts = 0;
+    let mins = minute || 0;
+    this.hours = hour < 0 ? 24 - (Math.abs(hour) % 24) : hour % 24;
+    mins >= 0 ? this.plus(mins) : this.minus(mins);
   }
 
   public toString(): string {
@@ -15,34 +17,43 @@ export class Clock {
   }
 
   public plus(minutes: number): Clock {
+    console.log(this.hours);
+
     while (minutes >= 60) {
       minutes = minutes - 60;
+      console.log(this.hours);
       this.hours++;
+      this.hours = this.hours % 24;
     }
     if (this.minuts + minutes >= 60) {
       this.hours++;
       this.minuts = minutes + this.minuts - 60;
+      this.hours = this.hours % 24;
       return this;
     }
     this.minuts = minutes + this.minuts;
     return this;
   }
 
-  public minus(minutes: number): Clock {
+  public minus(minut: number): Clock {
+    let minutes: number = minut >= 0 ? minut : Math.abs(minut);
     while (minutes >= 60) {
       minutes = minutes - 60;
       this.hours--;
+      this.hours = this.hours < 0 ? 24 + this.hours : this.hours % 24;
     }
-    if (this.minuts <= minutes) {
+    if (this.minuts < minutes) {
       this.hours--;
+      minutes = minutes - this.minuts;
       this.minuts = 60 - minutes;
+      this.hours = this.hours < 0 ? 24 + this.hours : this.hours % 24;
       return this;
     }
     this.minuts = this.minuts - minutes;
     return this;
   }
 
-  public equals(hour: number, minute?: number): Clock {
-    return new Clock(hour, minute);
+  public equals(other: Clock): boolean {
+    return this.toString() === other.toString();
   }
 }
