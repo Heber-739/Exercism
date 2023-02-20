532016ca5635569e8489d5a7cced5e3b4bee9926
Heber-739 <Heberduarteryr@gmail.com>
2023-02-19 22:17:38 -0300

    18th exercism typescript

--- Typescript/18-linked-list.ts
+++ Typescript/18-linked-list.ts
@@ -1,25 +1,138 @@
-export class LinkedList<TElement> {
-  public push(element: unknown) {
-    throw new Error("Remove this statement and implement this function");
-  }
-
-  public pop(): unknown {
-    throw new Error("Remove this statement and implement this function");
-  }
-
-  public shift(): unknown {
-    throw new Error("Remove this statement and implement this function");
-  }
-
-  public unshift(element: unknown) {
-    throw new Error("Remove this statement and implement this function");
-  }
-
-  public delete(element: unknown) {
-    throw new Error("Remove this statement and implement this function");
-  }
-
-  public count(): unknown {
-    throw new Error("Remove this statement and implement this function");
+export class Node {
+  public info: any;
+  public next: Node | null;
+  public before: Node | null;
+  constructor(info: any) {
+    this.info = info;
+    this.next = null;
+    this.before = null;
   }
 }
+
+export class LinkedList<TElement> {
+  private inicio: Node | null;
+  private fin: Node | null;
+
+  constructor() {
+    this.inicio = null;
+    this.fin = null;
+  }
+
+  private empty() {
+    return this.inicio == null && this.fin == null ? true : false;
+  }
+
+  public push(element: TElement) {
+    let node: Node = new Node(element);
+    if (this.empty()) {
+      this.inicio = node;
+      this.fin = node;
+    } else {
+      this.fin!.next = node;
+      node.before = this.fin;
+    }
+    this.fin = node;
+  }
+
+  public pop(): TElement {
+    if (this.inicio == this.fin) {
+      let au = this.inicio?.info;
+      this.inicio = this.fin = null;
+      return au;
+    }
+    let aux: Node = this.fin!;
+    this.fin = this.fin!.before;
+    this.fin!.next = null;
+    aux.before = aux.next = null;
+    return aux.info;
+  }
+
+  public shift(): TElement {
+    if (this.inicio == this.fin) {
+      let au = this.inicio?.info;
+      this.inicio = this.fin = null;
+      return au;
+    }
+    let aux: Node = this.inicio!;
+    this.inicio = this.inicio!.next;
+    this.inicio!.before = null;
+    aux.before = aux.next = null;
+    return aux.info;
+  }
+
+  public unshift(element: TElement) {
+    let node: Node = new Node(element);
+    if (this.empty()) {
+      this.inicio = node;
+      this.fin = node;
+    } else {
+      node.next = this.inicio;
+      this.inicio!.before = node;
+    }
+    this.inicio = node;
+  }
+
+  public delete(element: TElement) {
+    if (this.inicio === this.fin && this.inicio?.info === element) {
+      this.inicio = this.fin = null;
+    }
+    let aux: Node | null = this.inicio!;
+    while (aux != null) {
+      if (aux.info === element) {
+        if (aux.next) {
+          let aux2: Node | null = aux.next;
+          aux.before!.next = aux2;
+          aux2.before = aux.before;
+        } else {
+          aux.before!.next = null;
+        }
+        aux = null;
+      } else {
+        if (aux?.next) {
+          aux = aux!.next;
+        } else {
+          break;
+        }
+      }
+    }
+  }
+
+  public count(): number {
+    let i = this.inicio;
+    let long: number = 0;
+    while (i != null) {
+      long++;
+      if (i.next) {
+        i = i?.next!;
+      } else {
+        break;
+      }
+    }
+    return long;
+  }
+}
+/* ----------------- Pruebas ------------------- */
+const list = new LinkedList<number>();
+list.push(10);
+list.push(20);
+console.log(list.pop());
+list.pop(); /* (20) */
+list.push(30);
+list.shift(); /* (10) */
+list.unshift(40);
+list.push(50);
+list.shift(); /* 40) */
+list.pop(); /* 50) */
+list.shift(); /* 30) */
+
+/* [30,50] */
+
+/*
+ListaDoblementeEnlazada listita = new ListaDoblementeEnlazada();
+
+        push( insertar valor al dorso );
+pop( quitar valor al dorso );
+
+shift( quitar valor al frente ).
+unshift( insertar valor al frente );
+         */
--- Typescript/pizarra.ts
+++ Typescript/pizarra.ts
@@ -1,121 +1,66 @@
-export class Node {
-  public info: any;
-  public next: Node | null;
-  public before: Node | null;
-  constructor(info: any) {
-    this.info = info;
-    this.next = null;
-    this.before = null;
-  }
-}
-
-export class LinkedList<TElement> {
-  private inicio: Node | null;
-  private fin: Node | null;
-
-  constructor() {
-    this.inicio = null;
-    this.fin = null;
+export class Rational {
+  private num: number;
+  private den: number;
+  constructor(numerator: number, denominator: number) {
+    this.num = numerator;
+    this.den = denominator;
   }
 
-  private empty() {
-    return this.inicio == null && this.fin == null ? true : false;
-  }
-
-  public push(element: TElement) {
-    let node: Node = new Node(element);
-    if (this.empty()) {
-      this.inicio = node;
-      this.fin = node;
-    } else {
-      this.fin!.next = node;
-      node.before = this.fin;
+  public add(a: number, b: number): Rational {
+    if (this.den === b) {
+      return this.reduce(new Rational(this.num + a, b));
     }
-    this.fin = node;
+    return this.reduce(new Rational(this.num * b + this.den * a, this.den * b));
   }
 
-  public pop(): TElement {
-    if (this.inicio == this.fin) {
-      let au = this.inicio;
-      this.inicio = this.fin = null;
-      return au.info!;
+  public sub(a: number, b: number) {
+    if (this.den === b) {
+      return this.reduce(new Rational(this.num - a, b));
     }
-    let aux: Node = this.fin!;
-    this.fin = this.fin!.before;
-    this.fin!.next = null;
-    aux.before = aux.next = null;
-    return aux.info;
+    return this.reduce(new Rational(this.num * b - this.den * a, this.den * b));
   }
 
-  public shift(): TElement {
-    let aux: Node = this.inicio!;
-    this.inicio = this.inicio!.next;
-    this.inicio!.before = null;
-    aux.before = aux.next = null;
-    return aux.info;
+  public mul(a: number, b: number) {
+    return this.reduce(new Rational(this.num * a, this.den * b));
   }
 
-  public unshift(element: TElement) {
-    let node: Node = new Node(element);
-    if (this.empty()) {
-      this.inicio = node;
-      this.fin = node;
-    } else {
-      node.next = this.inicio;
-      this.inicio!.before = node;
+  public div(a: number, b: number) {
+    return this.reduce(new Rational(this.num * b, this.den * a));
+  }
+
+  public abs() {
+    this.num = this.num < 0 ? this.num * -1 : this.num;
+    this.den = this.den < 0 ? this.den * -1 : this.den;
+    return new Rational(this.num, this.den);
+  }
+
+  public exprational() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  public expreal() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  private reduce(rat: Rational): Rational {
+    if (rat.num % rat.den === 0 || rat.den % rat.num === 0) {
+      return rat.num % rat.den === 0
+        ? new Rational(rat.num / rat.den, 1)
+        : new Rational(1, rat.den / rat.num);
     }
-    this.inicio = node;
-  }
-
-  public delete(element: TElement) {
-    if (this.inicio === this.fin) {
-      return this.inicio!.info;
-    }
-    let aux: Node = this.inicio!;
-    while (aux != null) {
-      if (aux.info == element) {
-        if (aux.next) {
-          aux.before!.next = aux.next!.before;
-        } else {
-          aux.before!.next = null;
+    let gcd = (a: number, b: number, reverse: boolean): Rational => {
+      for (let i = a - 1; i > 0; i--) {
+        if (a % i === 0 && b % i === 0) {
+          if (reverse) {
+            return new Rational(b / i, a / i);
+          }
+          return new Rational(a / i, b / i);
         }
       }
-      aux = aux.next!;
-    }
-  }
-
-  public count(): number {
-    let i = this.inicio;
-    let long: number = 1;
-    while (i!.next) {
-      long++;
-      i = i?.next!;
-    }
-    return long;
+      return rat;
+    };
+    return rat.num > rat.den
+      ? gcd(rat.num, rat.den, false)
+      : gcd(rat.den, rat.num, true);
   }
 }
-/* ----------------- Pruebas ------------------- */
-const list = new LinkedList<number>();
-list.push(10);
-list.push(20);
-console.log(list.pop());
-list.pop(); /* (20) */
-list.push(30);
-list.shift(); /* (10) */
-list.unshift(40);
-list.push(50);
-list.shift(); /* 40) */
-list.pop(); /* 50) */
-list.shift(); /* 30) */
-
-/* [30,50] */
-
-/*
-ListaDoblementeEnlazada listita = new ListaDoblementeEnlazada();
-
-        push( insertar valor al dorso );
-pop( quitar valor al dorso );
-
-shift( quitar valor al frente ).
-unshift( insertar valor al frente );
-         */
