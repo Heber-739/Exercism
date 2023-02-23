c165af6bcf8e6073185afbdba07fd82adeaf117b
Heber-739 <Heberduarteryr@gmail.com>
2023-02-22 22:12:47 -0300

    22th exercism typescript

--- Typescript/pizarra.ts
+++ Typescript/pizarra.ts
@@ -1,55 +1,101 @@
-export class SimpleCipher {
-  public alphabet: string = "abcdefghijklmnopqrstuvwxyz";
-  public key: string;
+let operations: { [key: string]: string } = {
+  plus: "+",
+  minus: "-",
+  multiplied: "*",
+  divided: "/",
+  raised: "**",
+};
 
-  constructor(k?: string) {
-    this.key = k ? k : this.getKey();
-  }
-  public getKey(): string {
-    let newKey: string = "";
-    for (let i = 0; i < Math.round(Math.random() * 10 + 100); i++) {
-      newKey = newKey.concat(this.alphabet[Math.round(Math.random() * 26)]);
-    }
-    return newKey;
-  }
-  public encode(text: string): string {
-    let textEncoded: string = "";
-    for (let i = 0; i < text.length; i++) {
-      let ind: number = this.alphabet.indexOf(text[i]);
-      let index: number = this.alphabet.indexOf(
-        this.key[i - this.key.length * Math.trunc(i / this.key.length)]
-      );
-      textEncoded = textEncoded.concat(
-        this.alphabet[ind + index - 26 * Math.trunc((ind + index) / 26)]
-      );
-    }
-    return textEncoded;
-  }
+export const answer = (input: string): number | never => {
+  let text: string[] = input.replace("?", "").split(" ");
+  let op: boolean = false;
+  console.log(text);
+  let ev: string = "";
+  for (let i = 0; i < text.length; i++) {
+    let e = text[i];
 
-  public decode(text: string): string {
-    let textDecoded: string = "";
-    for (let i = 0; i < text.length; i++) {
-      let ind: number = this.alphabet.indexOf(text[i]);
-      let index: number = this.alphabet.indexOf(
-        this.key[i - this.key.length * Math.trunc(i / this.key.length)]
-      );
-      textDecoded = textDecoded.concat(
-        this.alphabet[ind - index + 26 * Math.ceil((index - ind) / 26)]
-      );
+    if (e.match(/[\d]+/)) {
+      try {
+        ev = `(${eval(ev.concat("(", e, ")"))})`;
+        op = false;
+      } catch (error) {}
     }
-    return textDecoded;
+
+    if (operations[e]) {
+      ev = ev.concat(operations[e]);
+      op = true;
+      if (e === "raised") {
+        for (let ind = i; ind < text.length; ind++) {
+          const el = text[ind];
+          if (el.match(/[\d]+/)) {
+            ev = ev.concat(el.match(/[\d]+/)![0]);
+            break;
+          }
+        }
+        break;
+      }
+    }
+    if (op && i + 1 == text.length) {
+      throw new Error("Unknown operation");
+    }
   }
-}
-const leti = new SimpleCipher("iamapandabear"); /* 'qayaeaagaciai' */
-console.log(Math.ceil(2.1));
-console.log(leti.encode("iamapandabear"));
-console.log(leti.decode("qayaeaagaciai"));
+  return eval(ev) ? eval(ev) : new Error("Unknown operation");
+};
+console.log(answer("What is -3 plus 7 multiplied by -2?"));
 
 /* 
-(new SimpleCipher('abc').encode('i a m a p andabear')).toEqual('i b o a q cnecbfcr')
-    
 
-Dada la clave "aaaaaaaaaaaaaaaaaa", codificar la cadena "iamapandabear" devolvería el "iamapandabear" original.
+Instrucciones
+Analice y evalúe problemas matemáticos sencillos que devuelvan la respuesta como un número entero.
 
-Dada la clave "dddddddddddddddddd", codificar nuestra cadena "iamapandabear" devolvería el oscurecido "ldpdsdqgdehdu"
+Iteración 0 — Números
+Los problemas sin operaciones simplemente se evalúan al número dado.
+
+¿Qué es 5?
+
+Evalúa a 5.
+
+Iteración 1 — Suma
+Suma dos números juntos.
+
+¿Cuánto es 5 más 13?
+Evalúa a 18.
+
+Manejar números grandes y números negativos.
+
+Iteración 2 — Resta, multiplicación y división
+Ahora, realiza las otras tres operaciones.
+
+¿Cuánto es 7 menos 5?
+2
+
+¿Cuánto es 6 multiplicado por 4?
+24
+
+¿Cuánto es 25 dividido por 5?
+5
+
+Iteración 3: operaciones múltiples
+Manejar un conjunto de operaciones, en secuencia.
+
+Dado que estos son problemas verbales, evalúe la expresión de izquierda a derecha, ignorando el orden típico de las operaciones.
+
+¿Cuánto es 5 más 13 más 6?
+24
+
+¿Cuánto es 3 más 2 multiplicado por 3?
+15 (es decir, no 9)
+
+Iteración 4 — Errores
+El analizador debe rechazar:
+
+Operaciones no admitidas ("¿Cuánto es 52 al cubo?")
+Preguntas no matemáticas ("¿Quién es el presidente de los Estados Unidos?")
+Problemas verbales con sintaxis no válida ("¿Cuánto es 1 más 2?")
+Bonus — Exponentials
+Si lo desea, maneje exponenciales.
+
+¿Cuánto es 2 elevado a la quinta potencia?
+
+32
 */
