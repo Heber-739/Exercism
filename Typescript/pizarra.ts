57b9096ff228ce2705bcb67e9f1fcb4e97af9efb
Heber-739 <Heberduarteryr@gmail.com>
2023-02-25 23:22:00 -0300

    28th exercism typescript

--- Typescript/pizarra.ts
+++ Typescript/pizarra.ts
@@ -1,4 +1,41 @@
-export function reverse(text: string): string {
-  return text.split("").reverse().join("");
+export class Triangle {
+  private isTriangle: boolean;
+  private sides: number[] = [];
+
+  constructor(a: number, b: number, c: number) {
+    if (a <= 0 || b <= 0 || b <= 0) {
+      this.isTriangle = false;
+    } else {
+      if (a + b >= c && b + c >= a && c + a >= b) {
+        this.isTriangle = true;
+      } else {
+        this.isTriangle = false;
+      }
+    }
+    this.sides = [a, b, c];
+  }
+
+  get isEquilateral(): boolean {
+    return this.isTriangle
+      ? this.sides[0] == this.sides[1] && this.sides[0] == this.sides[2]
+      : false;
+  }
+
+  get isIsosceles(): boolean {
+    return this.isTriangle
+      ? this.sides[0] == this.sides[1] ||
+          this.sides[0] == this.sides[2] ||
+          this.sides[1] == this.sides[2]
+      : false;
+  }
+
+  get isScalene() {
+    return this.isTriangle
+      ? this.sides[0] != this.sides[1] &&
+          this.sides[0] != this.sides[2] &&
+          this.sides[1] != this.sides[2]
+      : false;
+  }
 }
-console.log(reverse(`I'm hungry!'`));
+const tr: Triangle = new Triangle(2, 4, 7);
+console.log(tr.isScalene);
