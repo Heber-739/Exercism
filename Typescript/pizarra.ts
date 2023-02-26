73c51b179aeea8277c1f63e3f64b1b75adddd982
Heber-739 <Heberduarteryr@gmail.com>
2023-02-26 00:52:41 -0300

    32nd exercism typescript

--- Typescript/pizarra.ts
+++ Typescript/pizarra.ts
@@ -1,37 +1,24 @@
-const proteins: { [key: string]: string } = {
-  AUG: "Methionine",
-  UUU: "Phenylalanine",
-  UUC: "Phenylalanine",
-  UUA: "Leucine",
-  UUG: "Leucine",
-  UCU: "Serine",
-  UCC: "Serine",
-  UCA: "Serine",
-  UCG: "Serine",
-  UAU: "Tyrosine",
-  UAC: "yrosine",
-  UGU: "Cysteine",
-  UGC: "Cysteine",
-  UGG: "Tryptophan",
-  UAA: "STOP",
-  UAG: "STOP",
-  UGA: "STOP",
+const factors: { [key: number]: string } = {
+  3: "Pling",
+  5: "Plang",
+  7: "Plong",
 };
-
-export function translate(text: string): string[] {
-  console.log(text.slice(0));
-  let p: string[] = [];
-  let word: string = "";
-  for (let i = 0; i < text.length; i++) {
-    word = word + text[i];
-    if (word.length == 3) {
-      if (proteins[word] == "STOP") break;
-      p.push(proteins[word]);
-      word = "";
-    }
+export function convert(n: number): number | string {
+  let r: string = "";
+  if (n % 3 == 0) {
+    r = r + "Pling";
   }
-  return p;
+  return r;
 }
+/* 
+Las reglas de raindropsson que si un número dado:
 
-const expected = ["Methionine", "Phenylalanine", "Tryptophan"];
-console.log(translate("AUGUUUUGG"));
+tiene 3 como factor, agregue 'Pling' al resultado.
+tiene 5 como factor, agregue 'Plang' al resultado.
+tiene 7 como factor, agregue 'Plong' al resultado.
+no tiene 3, 5 o 7 como factor, el resultado debe ser los dígitos del número.
+Ejemplos
+28 tiene 7 como factor, pero no 3 ni 5, por lo que el resultado sería "Plong".
+30 tiene 3 y 5 como factores, pero no 7, por lo que el resultado sería "PlingPlang".
+34 no está factorizado por 3, 5 o 7, por lo que el resultado sería "34".
+ */
