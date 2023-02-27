1e25c51c79d2f321e7b70e28648e2cbcb83f6331
Heber-739 <Heberduarteryr@gmail.com>
2023-02-27 20:17:00 -0300

    41st exercism typescript

--- Typescript/pizarra.ts
+++ Typescript/pizarra.ts
@@ -1,11 +1,34 @@
-export const square = (g: number): bigint => {
-  if (g <= 0 || g > 64) {
-    throw new Error("error");
-  }
-  return BigInt(2 ** --g);
+type Options = {
+  minFactor?: number;
+  maxFactor?: number;
+  sum: number;
 };
 
-export const total = () => BigInt(2 ** 64) - 1n;
-console.log(total().toString());
-console.log("18446744073709551615n".length);
-console.log(square(32)); /* 2147483648n */
+export function triplets(op: Options): Triplet[] {
+  let min: number = op.minFactor ? op.minFactor : 0;
+  let max: number = op.maxFactor ? op.maxFactor : op.sum;
+  let arr: Triplet[] = [];
+  for (let i = min; i <= max - 2; i++) {
+    for (let j = i + 1; j < max - 1; j++) {
+      for (let k = j + 1; k < max; k++) {
+        if (i + j + k === op.sum) {
+          if (i ** 2 + j ** 2 == k ** 2) {
+            arr.push(new Triplet(i, j, k));
+          }
+        }
+      }
+    }
+  }
+  return arr;
+}
+
+class Triplet {
+  private arr: [number, number, number];
+  constructor(a: number, b: number, c: number) {
+    this.arr = [a, b, c];
+  }
+
+  toArray(): [number, number, number] {
+    return this.arr;
+  }
+}
