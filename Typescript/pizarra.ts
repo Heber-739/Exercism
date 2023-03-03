449f64f12721b1f51caf1e5ca60da367bdfc1758
Heber-739 <Heberduarteryr@gmail.com>
2023-03-02 22:12:34 -0300

    50th exercism typescript pending

--- Typescript/pizarra.ts
+++ Typescript/pizarra.ts
@@ -1,81 +1,56 @@
-export class TwoBucket {
-  private movs: number;
-  private goalBucke: string;
-  private otherBucke: number;
-
-  constructor(c1: number, c2: number, goal: number, start: string) {
-    /*        6           15             9                one*/
-    let mov: number = 1;
-    let st = start == "one" ? c1 : c2;
-    let aux1: number = start == "one" ? c1 : c2;
-    let aux2: number = start == "one" ? c2 : c1;
-    let ot: number = 0;
-    while (true) {
-      console.log(st, ot);
-      /*                   [5 0, 2 3, 2 0, 0 2, 5 2, 4 3, 4 0, 1 3]*/
-      if (st == goal) {
-        this.goalBucke = start == "one" ? "one" : "two";
-        this.otherBucke = ot;
-        break;
-      }
-      if (ot == goal) {
-        this.goalBucke = start == "one" ? "two" : "one";
-        this.otherBucke = st;
-        break;
-      }
-      if (st == aux1 && ot == 0) {
-        st = ot + aux1 > aux2 ? st - (aux2 - ot) : 0;
-        ot = ot + aux1 > aux2 ? aux2 : ot + aux1;
-      } else if (st == 0 && ot == aux2) {
-        st = aux1;
-      } else if (st == aux1 && ot < aux2 && ot > 0) {
-        st = ot + aux1 > aux2 ? st - (aux2 - ot) : 0;
-        ot = ot + aux1 > aux2 ? aux2 : ot + aux1;
-      } else if (st < aux1 && st > 0 && ot == aux2) {
-        ot = 0;
-      } else if (st < aux1 && st > 0 && ot == 0) {
-        let ap: number = st;
-        st = ot + st > aux2 ? st - (aux2 - ot) : 0;
-        ot = ot + ap > aux2 ? aux2 : ot + ap;
-      } else if (st == 0 && ot < aux2) {
-        st = aux1;
-      } else if (st == aux1 && ot == aux2) {
-        throw new Error("e");
-      } else if (mov > 10) {
-        throw new Error("d");
-      }
-      /*[5 0, 2 3, 2 0, 0 2, 5 2, 4 3, 4 0, 1 3]*/
-      mov++;
+let tr: { [key: string]: number } = {
+  0: 0,
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  a: 10,
+  b: 11,
+  c: 12,
+  d: 13,
+  e: 14,
+  f: 15,
+};
+export function encode(values: string[]): string[] {
+  let arr: string[] = [];
+  for (let j = 0; j < values.length; j++) {
+    const i = values[j];
+    let n: number = 0;
+    let hex = i.slice(2).split("").reverse();
+    for (let i = 0; i < hex.length; i++) {
+      n += tr[hex[i]] * 16 ** i;
     }
-    this.movs = mov;
+    console.log(n);
+    for (let i = 3; i >= 0; i--) {
+      console.log(n.toString(16));
+      if (n >= 128 ** i) {
+        let integer = Math.trunc(n / 128 ** i);
+        console.log(n);
+        let sum: number = i > 0 ? 128 : 0;
+        let pre: string = integer > 9 || i >= 1 ? "0x" : "";
+        arr.push(pre + (integer + sum).toString(16));
+        n -= integer * 128 ** i;
+      } else {
+        let pre: string = i >= 1 ? "0x80" : "0";
+        arr.push(pre);
+      }
+    }
   }
-
-  moves() {
-    return this.movs;
-  }
-
-  get goalBucket() {
-    return this.goalBucke;
-  }
-
-  get otherBucket() {
-    return this.otherBucke;
-  }
+  values.forEach((i) => {});
+  return arr;
 }
-const tb = new TwoBucket(6, 15, 9, "two"); // 8
-console.log(tb.moves(), tb.goalBucket, tb.otherBucket);
 
-/* 6 15,
-           [5 0, 2 3, 2 0, 0 2, 5 2, 4 3, 4 0, 1 3]
-*/
-
+export function decode() {
+  throw new Error("Remove this statement and implement this function");
+}
+console.log(encode(["0x4000"]));
 /* 
-      expect(twoBucket.moves()).toEqual(14)
-      expect(twoBucket.goalBucket).toEqual('one')
-      expect(twoBucket.otherBucket).toEqual(11)
-
-      const starterBuck = 'two'
-      expect(twoBucket.moves()).toEqual(18)
-      expect(twoBucket.goalBucket).toEqual('two')
-      expect(twoBucket.otherBucket).toEqual(7)
+      [0x4000, 0x123456])).toEqual([
+        0x81, 0x80, 0, 0xc8, 0xe8, 0x56,
+      ])
        */
