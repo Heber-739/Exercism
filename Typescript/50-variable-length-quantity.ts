c8673cc9d950bbb0fa44d76a83fc7b4c069caf4b
Heber-739 <Heberduarteryr@gmail.com>
2023-03-03 15:29:50 -0300

    50th exercism typescript

---
+++ Typescript/50-variable-length-quantity.ts
@@ -0,0 +1,71 @@
+export function encode(values: number[]): number[] {
+  let res: number[] = [];
+  for (let j = 0; j < values.length; j++) {
+    let arr: number[] = [];
+    let n: number = values[j];
+    if (n == 0) {
+      arr.push(0);
+    } else {
+      for (let i = 4; i >= 0; i--) {
+        if (n >= 128 ** i) {
+          let integer = Math.trunc(n / 128 ** i);
+          let sum: number = i > 0 ? 128 : 0;
+          n -= integer * 128 ** i;
+          arr.push(integer + sum);
+        } else if (n == 0) {
+          let sum: number = i > 0 ? 128 : 0;
+          arr.push(sum);
+        }
+      }
+    }
+    res = res.concat(arr);
+  }
+  return res;
+}
+
+export function decode(i: number[]): number[] {
+  let aux: number[] = [];
+  let values: Array<number[]> = [];
+  i.forEach((e) => {
+    aux.push(e);
+    if (e < 128) {
+      values.push(aux);
+      aux = [];
+    }
+  });
+  if (values.length == 0) {
+    throw new Error("Incomplete sequence");
+  }
+
+  values.forEach((e) => {
+    let ar: number[] = e.reverse();
+    let n: number = 0;
+    for (let i = 0; i <= ar.length - 1; i++) {
+      let k: number = i > 0 ? e[i] - 128 : e[i];
+      console.log(k);
+      n += k * 128 ** i;
+    }
+    aux.push(n);
+  });
+  return aux;
+}
+let arr = [
+  0xc0, 0, 0xc8, 0xe8, 0x56, 0xff, 0xff, 0xff, 0x7f, 0, 0xff, 0x7f, 0x81, 0x80,
+  0,
+];
+console.log(decode(arr));
+/* 8192,
+  -   1193046,
+  -   268435455,
+  -   0,
+  -   16383,
+  -   16384, */
+/* console.log(encode([0xff]));
+  console.log(parseInt(arr[12].toString())) */
+/* console.log((0x4000).toString(8));
+ */
+/* 
+        [0x4000, 0x123456])).toEqual([
+          0x81, 0x80, 0, 0xc8, 0xe8, 0x56,
+        ])
+         */
--- Typescript/pizarra.ts
+++ Typescript/pizarra.ts
@@ -1,54 +1,69 @@
-let tr: { [key: string]: number } = {
-  0: 0,
-  1: 1,
-  2: 2,
-  3: 3,
-  4: 4,
-  5: 5,
-  6: 6,
-  7: 7,
-  8: 8,
-  9: 9,
-  a: 10,
-  b: 11,
-  c: 12,
-  d: 13,
-  e: 14,
-  f: 15,
-};
-export function encode(values: string[]): string[] {
-  let arr: string[] = [];
+export function encode(values: number[]): number[] {
+  let res: number[] = [];
   for (let j = 0; j < values.length; j++) {
-    const i = values[j];
-    let n: number = 0;
-    let hex = i.slice(2).split("").reverse();
-    for (let i = 0; i < hex.length; i++) {
-      n += tr[hex[i]] * 16 ** i;
-    }
-    console.log(n);
-    for (let i = 3; i >= 0; i--) {
-      console.log(n.toString(16));
-      if (n >= 128 ** i) {
-        let integer = Math.trunc(n / 128 ** i);
-        console.log(n);
-        let sum: number = i > 0 ? 128 : 0;
-        let pre: string = integer > 9 || i >= 1 ? "0x" : "";
-        arr.push(pre + (integer + sum).toString(16));
-        n -= integer * 128 ** i;
-      } else {
-        let pre: string = i >= 1 ? "0x80" : "0";
-        arr.push(pre);
+    let arr: number[] = [];
+    let n: number = values[j];
+    if (n == 0) {
+      arr.push(0);
+    } else {
+      for (let i = 4; i >= 0; i--) {
+        if (n >= 128 ** i) {
+          let integer = Math.trunc(n / 128 ** i);
+          let sum: number = i > 0 ? 128 : 0;
+          n -= integer * 128 ** i;
+          arr.push(integer + sum);
+        } else if (n == 0) {
+          let sum: number = i > 0 ? 128 : 0;
+          arr.push(sum);
+        }
       }
     }
+    res = res.concat(arr);
   }
-  values.forEach((i) => {});
-  return arr;
+  return res;
 }
 
-export function decode() {
-  throw new Error("Remove this statement and implement this function");
+export function decode(i: number[]): number[] {
+  let aux: number[] = [];
+  let values: Array<number[]> = [];
+  i.forEach((e) => {
+    aux.push(e);
+    if (e < 128) {
+      values.push(aux);
+      aux = [];
+    }
+  });
+  if (values.length == 0) {
+    throw new Error("Incomplete sequence");
+  }
+
+  values.forEach((e) => {
+    let ar: number[] = e.reverse();
+    let n: number = 0;
+    for (let i = 0; i <= ar.length - 1; i++) {
+      let k: number = i > 0 ? e[i] - 128 : e[i];
+      console.log(k);
+      n += k * 128 ** i;
+    }
+    aux.push(n);
+  });
+  return aux;
 }
-console.log(encode(["0x4000"]));
+let arr = [
+  0xc0, 0, 0xc8, 0xe8, 0x56, 0xff, 0xff, 0xff, 0x7f, 0, 0xff, 0x7f, 0x81, 0x80,
+  0,
+];
+console.log(decode(arr));
+/* 8192,
+-   1193046,
+-   268435455,
+-   0,
+-   16383,
+-   16384, */
+/* console.log(encode([0xff]));
+console.log(parseInt(arr[12].toString())) */
+/* console.log((0x4000).toString(8));
+ */
 /* 
       [0x4000, 0x123456])).toEqual([
         0x81, 0x80, 0, 0xc8, 0xe8, 0x56,
