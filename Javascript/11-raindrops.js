74b0d17d5aed9496b3b6f71faed34c6fb254382d
Heber-739 <Heberduarteryr@gmail.com>
2023-03-11 21:37:59 -0300

    11th exercism javascript

---
+++ Javascript/11-raindrops.js
@@ -0,0 +1,21 @@
+export const convert = (nro) => {
+  let res = "";
+  res = nro % 3 == 0 ? res.concat("Pling") : res;
+  res = nro % 5 == 0 ? res.concat("Plang") : res;
+  res = nro % 7 == 0 ? res.concat("Plong") : res;
+  return res.length > 0 ? res : "" + nro;
+};
+
+console.log(typeof convert(1));
+/* 
+  
+  tiene 3 como factor, agregue 'Pling' al resultado.
+  tiene 5 como factor, agregue 'Plang' al resultado.
+  tiene 7 como factor, agregue 'Plong' al resultado.
+  no tiene 3, 5 o 7 como factor, el resultado debe ser los dígitos del número.
+   xtest('the sound for 105 is PlingPlangPlong as it has factors 3, 5 and 7', () =>
+      expect(convert(105)).toEqual('PlingPlangPlong'));
+    xtest('the sound for 3125 is Plang as it has a factor 5', () =>
+      expect(convert(3125)).toEqual('Plang'));
+  
+  */
--- Javascript/pizarra.js
+++ Javascript/pizarra.js
@@ -1,19 +1,21 @@
-export const rows = (rws) => {
-  let res = rws > 0 ? [[1]] : [];
-  for (let i = 1; i < rws; i++) {
-    let row = [];
-    let arr = res[i - 1];
-    for (let j = 0; j < i; j++) {
-      if (arr[j - 1] && arr[j]) {
-        row.push(arr[j - 1] + arr[j]);
-      } else {
-        row.push(1);
-      }
-    }
-    row.push(1);
-    res.push(row);
-  }
-  return res;
+export const convert = (nro) => {
+  let res = "";
+  res = nro % 3 == 0 ? res.concat("Pling") : res;
+  res = nro % 5 == 0 ? res.concat("Plang") : res;
+  res = nro % 7 == 0 ? res.concat("Plong") : res;
+  return res.length > 0 ? res : "" + nro;
 };
 
-console.log(rows(7));
+console.log(typeof convert(1));
+/* 
+
+tiene 3 como factor, agregue 'Pling' al resultado.
+tiene 5 como factor, agregue 'Plang' al resultado.
+tiene 7 como factor, agregue 'Plong' al resultado.
+no tiene 3, 5 o 7 como factor, el resultado debe ser los dígitos del número.
+ xtest('the sound for 105 is PlingPlangPlong as it has factors 3, 5 and 7', () =>
+    expect(convert(105)).toEqual('PlingPlangPlong'));
+  xtest('the sound for 3125 is Plang as it has a factor 5', () =>
+    expect(convert(3125)).toEqual('Plang'));
+
+*/
