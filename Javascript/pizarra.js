1a1d90a7a7dd269b853635950d7e0cd4854e9d55
Heber-739 <Heberduarteryr@gmail.com>
2023-03-14 09:43:04 -0300

    22nd exercism javascript

--- Javascript/pizarra.js
+++ Javascript/pizarra.js
@@ -1,32 +1,3 @@
-export function cookingStatus(timer) {
-  if (!timer) {
-    ("You forgot to set the timer.");
-  }
-  return timer == 0 ? "Lasagna is done." : "Not done, please wait.";
-}
-export function preparationTime(layers, timer) {
-  return timer ? layers.length * timer : layers.length * 2;
-}
-export function quantities(layers) {
-  let res = { noodles: 0, sauce: 0.0 };
-  layers.forEach((e) => {
-    if (e == "noodles") {
-      res[e] += 50;
-    }
-    if (e == "sauce") {
-      res[e] += 0.2;
-    }
-  });
-  return res;
-}
-export function addSecretIngredient(friendsList, myList) {
-  myList.push(friendsList[friendsList.length - 1]);
-}
-
-export function scaleRecipe(recipe, nr) {
-  let res = {};
-  for (const key in recipe) {
-    res[key] = (recipe[key] / 2) * nr;
-  }
-  return res;
-}
+export const twoFer = (t = "") => {
+  return t === "" ? "One for you, one for me." : `One for ${t}, one for me.`;
+};
